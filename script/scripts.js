b8b253cf485c05fb53c36d19d51020ae1f0f9811
hamazumi <khamamoto12@gmail.com>
2021-05-17 10:08:43 -0700

    Added moving player

--- script/scripts.js
+++ script/scripts.js
@@ -1,55 +1,93 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     //Variables
-    let canvas = document.querySelector('#game');
-    let background = new Image();
-
-    
-    //allows ability to draw
-    let draw = canvas.getContext('2d');
-
-    
-
-    //canvas
+    const SCALE = 0.5;
+    const WIDTH = 60;
+    const HEIGHT = 60;
+    const SCALED_WIDTH = SCALE * WIDTH;
+    const SCALED_HEIGHT = SCALE * HEIGHT;
+    const MOVEMENT_SPEED = 1;
 
 
-    //Start location
-     let xPos = 0;
-     let yPos = 0;
+    let canvas = document.getElementById ("canvas");
+    let ctx = canvas.getContext("2d");
+    let keyPresses = {};
+    let positionX = 0;
+    let positionY = 0;
+    let img = new Image();
 
-    // square
-    draw.fillRect(xPos, yPos, 50, 50);
-    draw.stroke();
-
-    //Function to move square
-    function move(e){
-
-        //Right Arrow Key
-        if(e.keyCode==39){
-            xPos+=5;
-        }
-        //Left Arrow Key
-        if(e.keyCode==37){
-            xPos-=5;
-        }
-        //Up Arrow Key
-        if(e.keyCode==38){
-            yPos-=5;
-        }
-        //Down Arrow Key
-        if(e.keyCode==40){
-            yPos+=5;
-        }
-
-        //Resets the canvas
-        canvas.width=canvas.width;
-        //redraw square
-        draw.fillRect(xPos, yPos, 50, 50);
-        draw.stroke();
-       
+    //TIMER
+    let timeDisplay = document.getElementById("timer");
+    let startButton = document.getElementById("startGame")
+    let timeLeft = 10
+    //Countdown Timer
+    function countDown(){
+        setInterval(function(){
+            if(timeLeft <= 0) {
+                clearInterval(timeLeft = 0)
+            }
+            timeDisplay.innerHTML = timeLeft
+            timeLeft -=1
+            
+        }, 1000);
     }
+    
+    startButton.addEventListener('click', countDown)
 
-    document.onkeydown = move;
+     //Event Listeners
+     window.addEventListener('keydown', keyDownListener, false);
+     function keyDownListener(event) {
+         keyPresses[event.key] = true;
+         console.log(event.key);
+     }
+ 
+     window.addEventListener('keyup', keyUpListener, false);
+     function keyUpListener(event) {
+         keyPresses[event.key] = false;
+ 
+     }
+     
+     //Load Initial Image 
+     function loadImage() {
+         img.src = 'images/6.64.png';
+         img.onload = function() {
+             // ctx.drawImage(img, 0, 0, 60, 60, 0, 0, 30, 30);
+             
+             gameLoop();
+         }
+     }
+ 
+     //Function to dynamically updatre frame
+     function drawFrame(frameX, frameY, canvasX, canvasY) {
+         ctx.drawImage(img, frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT, canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+     }
+ 
+     //Call to load initial image
+     loadImage();
+ 
+     //Function for player keyboard input
+     function gameLoop() {
+         //Always clears canvas first
+         ctx.clearRect(0, 0, canvas.width, canvas.height);
+         
+         //Event 
+         if (keyPresses.ArrowUp) {
+             positionY -= MOVEMENT_SPEED;
+         } else if (keyPresses.ArrowDown) {
+             positionY += MOVEMENT_SPEED;
+         }
+ 
+         if (keyPresses.ArrowLeft) {
+             positionX -= MOVEMENT_SPEED;
+         } else if (keyPresses.ArrowRight) {
+             positionX += MOVEMENT_SPEED;
+         }
+ 
+         drawFrame(0, 0, positionX, positionY);
+ 
+         window.requestAnimationFrame(gameLoop);
+     }
 
+    
 })
 
