c6473a94baf1cc4249ac927071cbd4d37fa552e0
hamazumi <khamamoto12@gmail.com>
2021-05-18 00:32:17 -0700

    Multiple Objects on Canvas

--- script/scripts.js
+++ script/scripts.js
@@ -1,41 +1,92 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    //Variables
-    const SCALE = 0.5;
-    const WIDTH = 60;
-    const HEIGHT = 60;
-    const SCALED_WIDTH = SCALE * WIDTH;
-    const SCALED_HEIGHT = SCALE * HEIGHT;
-    const MOVEMENT_SPEED = 1;
+    //DOM ELEMENTS
 
-
+    let startButton = document.getElementById("startGame")
+    let intro = document.getElementById('intro')
+    let main = document.getElementById('main')
     let canvas = document.getElementById ("canvas");
     let ctx = canvas.getContext("2d");
-    let keyPresses = {};
-    let positionX = 125;
-    let positionY = 50;
-    let img = new Image();
-    
-
-    //TIMER
     let timeDisplay = document.getElementById("timer");
-    let startButton = document.getElementById("startGame")
-    let timeLeft = 10
-    //Countdown Timer
-    function countDown(){
-        setInterval(function(){
-            if(timeLeft <= 0) {
-                clearInterval(timeLeft = 0)
-            }
-            timeDisplay.innerHTML = timeLeft
-            timeLeft -=1
-            
-        }, 1000);
+
+    //CREATE SPRITES
+    let bruceImg = new Image()
+    bruceImg.src = 'images/6.64.png'
+    let waterBottleImg = new Image()
+    waterBottleImg.src = 'images/water.png'
+
+    /*----- Variable Declarations -----*/
+    // Bruce Lee Constructor
+    function Player(x, y, width, height, speed, img) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.speed = speed
+        this.alive = true
+        this.img = img
+        this.render = function() {
+            ctx.drawImage(this.img, this.x, this.y)
+        }
+    }
+
+    // Water Bottle Constructor
+    function WaterBottle(x, y, width, height, img) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.alive = true
+        this.img = img
+        this.render = function() {
+            ctx.drawImage(this.img, this.x, this.y)
+        }
+        this.collision = function() {
+            if (bruce.x + bruce.width > this.x
+                && bruce.x < this.x + this.width
+                && bruce.y < this.y + this.height
+                && bruce.y + bruce.height > this.y
+                && this.alive) {
+                    this.alive = false
+                    console.log("collision detected in Water Bottle Constructor")
+                }
+        }
+    }
+
+    // Random X and Y coordinate generators
+    const generateX = (min, max) => {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min) + min)
     }
     
-    startButton.addEventListener('click', countDown)
+    const generateY = (min, max) => {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min) + min)
+    }
 
-     //Event Listeners
+    // Randomly spawn piece of garbage
+    const randomNum = () => {
+        return Math.floor(Math.random() * 2)
+    }
+
+    // Random Water Bottle Position
+    let randomX = generateX(20, 480)
+    let randomY = generateY(20, 480)
+
+    let randomBottle = randomNum()
+
+    //GAME PIECE CREATION
+    let bruce = new Player(125, 50, 30, 30, 1, bruceImg) 
+    let bottle = new WaterBottle(100, 100, 20, 20, waterBottleImg) 
+    // let bottle = new WaterBottle(randomX, randomY, 20, 20, waterBottleImg) 
+
+
+
+    //USER KEY EVENT LISTENERS
+    let keyPresses = {};
+
      window.addEventListener('keydown', keyDownListener, false);
      function keyDownListener(event) {
          keyPresses[event.key] = true;
@@ -47,55 +98,54 @@ window.addEventListener('DOMContentLoaded', () => {
          keyPresses[event.key] = false;
  
      }
-     
-     //Load Initial Image 
-     function loadImage() {
-         img.src = 'images/6.64.png';
-         img.onload = function() {
-             
-             gameLoop();
-         }
-     }
- 
-     //Function to dynamically updatre frame
-     function drawFrame(frameX, frameY, canvasX, canvasY) {
-         ctx.drawImage(img, frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT, canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
-     }
- 
-     //Call to load initial image
-     loadImage();
- 
-     //Function for player keyboard input
-     function gameLoop() {
-         //Always clears canvas first
-         ctx.clearRect(0, 0, canvas.width, canvas.height);
-         
-         //Event 
-         if (keyPresses.ArrowUp) {
-            if (positionY - MOVEMENT_SPEED > 0) {
-                positionY -= MOVEMENT_SPEED;
-            }
-         } else if (keyPresses.ArrowDown) {
-            if (positionY + SCALED_HEIGHT + MOVEMENT_SPEED < canvas.height) {
-                positionY += MOVEMENT_SPEED;
-            }
-         }
- 
-         if (keyPresses.ArrowLeft) {
-            if (positionX - MOVEMENT_SPEED > 0){
-                positionX -= MOVEMENT_SPEED;
-            }
-         } else if (keyPresses.ArrowRight) {
-            if (positionX + SCALED_WIDTH + MOVEMENT_SPEED < canvas.width) {
-                positionX += MOVEMENT_SPEED;
-            }         
+
+
+    //Function for player keyboard input
+    function gameLoop() {
+        //Always clears canvas first
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //render Bruce
+        bruce.render()
+        bottle.render()
+        
+        if (bottle.alive && randomBottle === 0) {
+            bottle.collision()
+            console.log("collision detected in Game Loop")
+
+       }
+        
+        //Game Event 
+        if (keyPresses.ArrowUp) {
+           if (bruce.y - bruce.speed > 0) {
+               bruce.y -= bruce.speed;
+           }
+        } else if (keyPresses.ArrowDown) {
+           if (bruce.y + bruce.height + bruce.speed < canvas.height) {
+               bruce.y += bruce.speed;
+           }
         }
- 
-         drawFrame(0, 0, positionX, positionY);
- 
-         window.requestAnimationFrame(gameLoop);
-     }
 
-    
+        if (keyPresses.ArrowLeft) {
+           if (bruce.x - bruce.speed > 0){
+               bruce.x -= bruce.speed;
+           }
+        } else if (keyPresses.ArrowRight) {
+           if (bruce.x + bruce.width + bruce.speed < canvas.width) {
+               bruce.x += bruce.speed;
+           }         
+       }
+
+        //drawFrame(0, 0, bruce.x, bruce.y);
+
+        window.requestAnimationFrame(gameLoop);
+    }
+
+    //START GAME
+
+    gameLoop();
+
+
+
+
 })
-
