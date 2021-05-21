01501e4ee1305a2cbdf7ebca87086219c5682cca
hamazumi <khamamoto12@gmail.com>
2021-05-20 17:28:02 -0700

    added bonus bottles

--- script/scripts.js
+++ script/scripts.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let interfacePts = document.getElementById('interfacePts')    
     let main = document.getElementById('main')
     let canvas = document.getElementById ('canvas');
-    let timeDisplay = document.getElementById('timer');
+    let timeDisplay = document.getElementById('timer'); 
     let player1 = document.getElementById('player1')
     
     
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let chickenSound = new Audio ("sounds/chicken-clucking-sound-1.mp3")
     let keyPresses = {};
     let score = 0;
-    let timeLeft = 9
+    let timeLeft = 14
     
     //Object Images
     let background = new Image()
@@ -37,8 +37,6 @@ window.addEventListener('DOMContentLoaded', () => {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);   
     }
 
-    //Image Contructors
-
     // Bruce Lee Constructor
     function Player(x, y, width, height, speed, img) {
         this.x = x
@@ -52,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Water Bottle Constructor AND Collision Dectection
+    // Water Bottles Constructor AND Collision Dectection
     function WaterBottle(x, y, width, height, img) {
         this.x = x
         this.y = y
@@ -76,8 +74,31 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
         }
     }
+ 
+    //Bonus bottles with NO chickens
+    function WaterBottle2(x, y, width, height, img) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.img = img
+        this.draw = function() {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        }
+        this.collision = function() {
+            if (bruce.x + bruce.width > this.x
+                && bruce.x < this.x + this.width
+                && bruce.y < this.y + this.height
+                && bruce.y + bruce.height > this.y) {
+                    this.x = generateX(0, 285);
+                    this.y = generateY(0, 135);
+                    score++;
+                    kickSound.play();
+                }
+        }
+    }
 
-    // Chicken Constructor AND Collision Dectection
+    //Chickens Constructor AND Collision Dectection
     function whiteChicken(x, y, width, height, img) {
         this.x = x
         this.y = y
@@ -101,28 +122,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function whiteChicken2(x, y, width, height, img) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.img = img
-        this.draw = function() {
-            ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
-        }
-        this.collision = function() {
-            if (bruce.x + bruce.width > this.x
-                && bruce.x < this.x + this.width
-                && bruce.y < this.y + this.height
-                && bruce.y + bruce.height > this.y) {
-                    chickenSound.play();
-                    this.x = generateX(0, 285);
-                    this.y = generateY(0, 135);
-                    score--;
-                    console.log("collision detected in Chicken Constructor")
-                }
-        }
-    }
 
     // Random X and Y coordinate generators
     const generateX = (min, max) => {
@@ -140,6 +139,18 @@ window.addEventListener('DOMContentLoaded', () => {
     // Random Water Bottle Position
     let randomX = generateX(0, 285)
     let randomY = generateY(0, 135)
+    //Bonus Bottles
+    let randomX2 = generateX(0, 285)
+    let randomY2 = generateY(0, 135)
+    //Bonus Bottles
+    let randomX3 = generateX(0, 285)
+    let randomY3 = generateY(0, 135)
+    //Bonus Bottles
+    let randomX4 = generateX(0, 285)
+    let randomY4 = generateY(0, 135)
+    //Bonus Bottles
+    let randomX5 = generateX(0, 285)
+    let randomY5 = generateY(0, 135)
 
     //Random chicken position
     let randomXc = generateX(0, 285)
@@ -149,16 +160,25 @@ window.addEventListener('DOMContentLoaded', () => {
     let randomXc2 = generateX(0, 285)
     let randomYc2 = generateY(0, 135)
 
+    //Random chicken #3
+    let randomXc3 = generateX(0, 285)
+    let randomYc3 = generateY(0, 135)
+
     //Characters 
     let bruce = new Player(170, 50, 30, 30, 2, bruceImg) 
     let bottle = new WaterBottle(randomX, randomY, 12, 12, waterBottleImg) 
+    let bottle2 = new WaterBottle2(randomX2, randomY2, 12, 12, waterBottleImg) 
+    let bottle3 = new WaterBottle2(randomX3, randomY3, 12, 12, waterBottleImg) 
+    let bottle4 = new WaterBottle2(randomX4, randomY4, 12, 12, waterBottleImg) 
+    let bottle5 = new WaterBottle2(randomX5, randomY5, 12, 12, waterBottleImg) 
     let chicken = new whiteChicken(randomXc, randomYc, 15, 15, chickenImg) 
-    let chicken2 = new whiteChicken2(randomXc2, randomYc2, 15, 15, chickenImg) 
+    let chicken2 = new whiteChicken(randomXc2, randomYc2, 15, 15, chickenImg) 
+    let chicken3 = new whiteChicken(randomXc3, randomYc3, 15, 15, chickenImg) 
 
     //RESET Game
     function reset(){
         score = 0
-        timeLeft = 9
+        timeLeft = 14
         player1.innerText = ("Score: " + score)
         interfacePts.innerText = score
         bruce = new Player(170, 50, 30, 30, 2, bruceImg) 
@@ -236,16 +256,35 @@ window.addEventListener('DOMContentLoaded', () => {
         
         //check for collision
         bottle.collision()
+        bottle2.collision()
+        bottle3.collision()
+        bottle4.collision()
+        bottle5.collision()
         chicken.collision()
         chicken2.collision()
+        chicken3.collision()
         
         //draw objects
         bruce.draw()
         bottle.draw()
         chicken.draw()
 
-        if(score >= 3){
+        if(score >= 5){
             chicken2.draw()
+        }
+
+        if(score >= 10){
+            chicken3.draw()
+        }
+
+        if(timeLeft <= 4){
+            bottle2.draw()
+            bottle3.draw()
+        } 
+
+        if(timeLeft <= 2){
+            bottle4.draw()
+            bottle5.draw()
         }
         
         //Check for game over 
